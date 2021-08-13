a4fe7109d2871325b1c2066d1d628eed128e42d5
Sebby80 <sebkm80@gmail.com>
2021-08-13 19:35:28 +0530

    LinkedIn clone first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
