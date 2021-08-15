646f69fcb27f3fb9348b2b873d148d025261155c
Sebby80 <sebkm80@gmail.com>
2021-08-15 19:51:39 +0530

    Designed left and right side widgets for home page.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,101 @@ import styled from "styled-components";
 import React from "react";
 
 const Main = (props) => {
-  return <Container>Main!</Container>;
+  return (
+    <Container>
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/images/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: main;
 `;
 
+const CommonCard = styled.div`
+  text-align: center;
+  background-color: #fff;
+  border-radius: 5px;
+  margin-bottom: 8px;
+  overflow: hidden;
+  position: relative;
+  border: none;
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  background: white;
+  margin: 0 0 8px;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.8);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        border-radius: 35px;
+        margin: 4px 0;
+        padding-left: 16px;
+        flex-grow: 1;
+        background-color: white;
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+        span {
+          color: #70b5f9;
+        }
+      }
+    }
+  }
+`;
 export default Main;
--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -2,11 +2,121 @@ import styled from "styled-components";
 import React from "react";
 
 const Rightside = (props) => {
-  return <Container>Right Side!</Container>;
+  return (
+    <Container>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" alt="" />
+        </Title>
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
+        <Recommendation>
+          View all recommendations
+          <img src="/images/right-icon.svg" alt="" />
+        </Recommendation>
+      </FollowCard>
+      <BannerCard>
+        <img src="/images/linkedin-banner.jpeg" alt="" />
+      </BannerCard>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: rightside;
+`;
+
+const FollowCard = styled.div`
+  text-align: center;
+  margin-bottom: 8px;
+  overflow: hidden;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  padding: 12px;
+`;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 16px;
+`;
+
+const FeedList = styled.ul`
+  margin-top: 12px;
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
+    position: relative;
+    font-size: 14px;
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+    button {
+      background-color: transparent;
+      color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      border-radius: 15px;
+      display: inline-flex;
+      justify-content: cneter;
+      align-items: center;
+      box-sizing: border-box;
+      font-weight: 600;
+      text-align: center;
+      outline: none;
+      max-height: 32px;
+      max-width: 480px;
+    }
+  }
+`;
+const Avatar = styled.div`
+  background-image: url("/images/followTag.svg");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 48px;
+  height: 48px;
+  margin-right: 8px;
+  background-color: rgb(197, 201, 208);
+  border-radius: 24px;
+`;
+
+const Recommendation = styled.a`
+  color: #0a66c2;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+`;
+const BannerCard = styled(FollowCard)`
+  img {
+    height: 100%;
+    width: 100%;
+  }
 `;
 
 export default Rightside;
