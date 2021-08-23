d5225d8de6696b026b65ec7f2240981164291343
Sebby80 <sebkm80@gmail.com>
2021-08-23 17:34:02 +0530

    Deleted unwanted image and edit in config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
 
+//Set up your firebase and provide the config details below
 const firebaseConfig = {
-  apiKey: "AIzaSyBwWuwKJ3YxQhh1-IP6cIveL_a3L9lck4E",
-  authDomain: "linkedin-clone-81e8d.firebaseapp.com",
-  projectId: "linkedin-clone-81e8d",
-  storageBucket: "linkedin-clone-81e8d.appspot.com",
-  messagingSenderId: "775142827390",
-  appId: "1:775142827390:web:e4f8170a33055872acbf2f",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
